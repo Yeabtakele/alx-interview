63aa6c8a2f3ed669692b70aa5053914c7553c451
yeabtakele <bereketabe434@gmail.com>
2024-08-18 17:11:21 +0300

    fexed_2d_matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,36 +1,20 @@
 #!/usr/bin/python3
-'''D matrix'''
-def get_matrix_from_user():
-    try:
-        num_rows = int(input("Enter the number of rows: "))
-        num_cols = int(input("Enter the number of columns: "))
-
-        matrix = []
-        for i in range(num_rows):
-            row = input(f"Enter row {i+1} (space-separated values): ")
-            row_values = [int(x) for x in row.split()]
-            matrix.append(row_values)  
-
-        return matrix
-    except EOFError:
-        print("No input provided. Using default values.")
-        
-        return [
-            [1, 2, 3],
-            [4, 5, 6],
-            [7, 8, 9]
-        ]
-
+'''2D matrix'''
 def rotate_2d_matrix(matrix):
-    return [list(reversed(i)) for i in zip(*matrix)]
+    n = len(matrix)
 
-matrix = get_matrix_from_user()
-print("Original matrix:")
+    # Step 1: Transpose the matrix
+    for i in range(n):
+        for j in range(i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    # Step 2: Reverse each row
+    for i in range(n):
+        matrix[i].reverse()
+
+# Driver program
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+rotate_2d_matrix(matrix)
 for row in matrix:
-    print(row)
-
-rotated_matrix = rotate_2d_matrix(matrix)
-print("\nRotated matrix:")
-for row in rotated_matrix:
-    print(row)
+     print(" ".join(map(str, row)))
 
