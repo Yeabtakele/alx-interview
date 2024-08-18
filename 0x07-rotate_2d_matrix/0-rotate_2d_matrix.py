d8032fae66f4c405c05f9d03ff1f43d4daab9c97
yeabtakele <bereketabe434@gmail.com>
2024-08-18 16:42:34 +0300

    well_done_2D_

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,24 +1,36 @@
 #!/usr/bin/python3
-'''2D matrix'''
+'''D matrix'''
+def get_matrix_from_user():
+    try:
+        num_rows = int(input("Enter the number of rows: "))
+        num_cols = int(input("Enter the number of columns: "))
 
+        matrix = []
+        for i in range(num_rows):
+            row = input(f"Enter row {i+1} (space-separated values): ")
+            row_values = [int(x) for x in row.split()]
+            matrix.append(row_values)  # Append as a list, not a tuple
+
+        return matrix
+    except EOFError:
+        print("No input provided. Using default values.")
+        # Provide a default matrix or handle the situation as needed
+        return [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]
+        ]
 
 def rotate_2d_matrix(matrix):
-    '''rotates a 2d matrix 90° clockwise
-    Returns: Nothing'''
-    left, right = 0, len(matrix) - 1
+    return [list(reversed(i)) for i in zip(*matrix)]
 
-    while left < right:
-        for i in range(right - left):
-            top, bottom = left, right
-            # save topleft  value
-            topLeft = matrix[top][left + i]
-            # move bottom left to top left
-            matrix[top][left + i] = matrix[bottom - i][left]
-            # move bottom right to bottom left
-            matrix[bottom - i][left] = matrix[bottom][right - i]
-            # move top right to bottom right
-            matrix[bottom][right - i] = matrix[top + i][right]
-            # move top left to top right
-            matrix[top + i][right] = topLeft
-        right -= 1
-        left += 1
+matrix = get_matrix_from_user()
+print("Original matrix:")
+for row in matrix:
+    print(row)
+
+rotated_matrix = rotate_2d_matrix(matrix)
+print("\nRotated matrix:")
+for row in rotated_matrix:
+    print(row)
+
